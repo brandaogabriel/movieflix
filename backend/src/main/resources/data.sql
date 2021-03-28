ca6afe678d07b1d82944fc68b96f875e548cb03d
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-28 15:02:36 -0300

    chore: add User, Role seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,3 +3,14 @@ INSERT INTO genre (name, create_moment, status) VALUES ('aventura', NOW(), true)
 INSERT INTO genre (name, create_moment, status) VALUES ('terror', NOW(), true);
 INSERT INTO genre (name, create_moment, status) VALUES ('romance', NOW(), true);
 
+-- Users
+INSERT INTO user (name, email, password, create_moment, status) VALUES ('Bob', 'bob@gmail.com', '123456', NOW(), true);
+INSERT INTO user (name, email, password, create_moment, status) VALUES ('Ana', 'ana@gmail.com', '123456', NOW(), true);
+
+-- Roles
+INSERT INTO role (authority) VALUES ('ROLE_VISITOR');
+INSERT INTO role (authority) VALUES ('ROLE_MEMBER');
+
+-- Users roles
+INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
