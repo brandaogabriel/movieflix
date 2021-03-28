57df1a4e688bb18bc3415b7ef67ad46a2090496b
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-28 13:52:57 -0300

    feat: add Genre Entitty and LogFields

---
+++ backend/src/main/java/com/devgabriel/movieflix/common/LogFields.java
@@ -0,0 +1,58 @@
+package com.devgabriel.movieflix.common;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class LogFields implements Serializable {
+
+  @CreatedDate
+  @Column(name = "create_moment", updatable = false)
+  @JsonProperty("create_moment")
+  private Instant createMoment = Instant.now();
+
+  @LastModifiedDate
+  @Column(name = "update_moment", insertable = false)
+  @JsonProperty("update_moment")
+  private Instant updateDate = Instant.now();
+
+  @JsonFormat(shape = JsonFormat.Shape.NUMBER)
+  private Boolean status = true;
+
+  public LogFields() {
+  }
+
+  public Instant getCreateMoment() {
+    return createMoment;
+  }
+
+  public void setCreateMoment(Instant createMoment) {
+    this.createMoment = createMoment;
+  }
+
+  public Instant getUpdateDate() {
+    return updateDate;
+  }
+
+  public void setUpdateDate(Instant updateDate) {
+    this.updateDate = updateDate;
+  }
+
+  public Boolean getStatus() {
+    return status;
+  }
+
+  public void setStatus(Boolean status) {
+    this.status = status;
+  }
+}
---
+++ backend/src/main/java/com/devgabriel/movieflix/entities/Genre.java
@@ -0,0 +1,63 @@
+package com.devgabriel.movieflix.entities;
+
+import com.devgabriel.movieflix.common.LogFields;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "genre")
+public class Genre extends LogFields {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  @OneToMany(mappedBy = "genre")
+  private final List<Movie> movies = new ArrayList<>();
+
+  public Genre() {
+  }
+
+  public Genre(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<Movie> getMovies() {
+    return movies;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Genre genre = (Genre) o;
+    return Objects.equals(id, genre.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
