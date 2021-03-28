46209e340dad6279fcbfaeb08afea4e1944d1fa4
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-28 14:22:03 -0300

    chore: add Genre seed

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- Genres
+INSERT INTO genre (name, create_moment, status) VALUES ('aventura', NOW(), true);
+INSERT INTO genre (name, create_moment, status) VALUES ('terror', NOW(), true);
+INSERT INTO genre (name, create_moment, status) VALUES ('romance', NOW(), true);
+
