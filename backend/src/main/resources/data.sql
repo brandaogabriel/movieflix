9491e9013ea103d80dd445999c488710f1fda98e
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-29 14:08:57 -0300

    chore: add seed for user permission

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -16,4 +16,6 @@ INSERT INTO role (authority) VALUES ('ROLE_ADMIN');
 -- Users roles
 INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO user_role (user_id, role_id) VALUES (3, 3);
