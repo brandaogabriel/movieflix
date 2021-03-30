a21b81237bfe7fab3adea8050aba79ec21ba6bbf
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-30 11:08:33 -0300

    feat: add find all movies paged and find movie by id

--- backend/src/main/java/com/devgabriel/movieflix/resources/MovieResource.java
+++ backend/src/main/java/com/devgabriel/movieflix/resources/MovieResource.java
@@ -3,11 +3,13 @@ package com.devgabriel.movieflix.resources;
 import com.devgabriel.movieflix.dtos.MovieDTO;
 import com.devgabriel.movieflix.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -15,6 +17,25 @@ public class MovieResource {
 
   @Autowired
   private MovieService service;
+
+  @GetMapping(value = "/api/test")
+  public ResponseEntity<List<MovieDTO>> findAll() {
+    List<MovieDTO> movies = service.findAll();
+    return ResponseEntity.ok().body(movies);
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<MovieDTO>> findAllPaged(
+          @RequestParam(value = "page", defaultValue = "0") Integer page,
+          @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+          @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+          @RequestParam(value = "orderBy", defaultValue = "title") String orderBy
+  ) {
+
+    PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+    Page<MovieDTO> list = service.findAllPaged(pageRequest);
+    return ResponseEntity.ok().body(list);
+  }
 
   @GetMapping(value = "/{id}")
   public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
--- backend/src/main/java/com/devgabriel/movieflix/services/MovieService.java
+++ backend/src/main/java/com/devgabriel/movieflix/services/MovieService.java
@@ -5,16 +5,34 @@ import com.devgabriel.movieflix.entities.Movie;
 import com.devgabriel.movieflix.repositories.MovieRepository;
 import com.devgabriel.movieflix.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
 
   @Autowired
   private MovieRepository repository;
+
+  @Transactional(readOnly = true)
+  public List<MovieDTO> findAll() {
+    List<Movie> movies = repository.findAll();
+    return movies.stream()
+            .map(MovieDTO::new)
+            .collect(Collectors.toList());
+  }
+
+  @Transactional(readOnly = true)
+  public Page<MovieDTO> findAllPaged(PageRequest pageRequest) {
+    Page<Movie> movies = repository.findAll(pageRequest);
+    return movies.map(MovieDTO::new);
+  }
 
   @Transactional(readOnly = true)
   public MovieDTO findById(Long id) {
